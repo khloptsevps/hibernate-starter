9dcf2fb271d52d5b6200007db894de3280f0252b
Petr Khloptsev <p.khloptsev@gmail.com>
2025-11-27 21:10:31 +1000

    Добавил логирование

--- src/main/java/ru/khloptsev/hibernate/starter/HibernateRunner.java
+++ src/main/java/ru/khloptsev/hibernate/starter/HibernateRunner.java
@@ -2,36 +2,46 @@ package ru.khloptsev.hibernate.starter;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.model.naming.PhysicalNamingStrategySnakeCaseImpl;
-import org.hibernate.cfg.Configuration;
-import ru.khloptsev.hibernate.starter.converter.BirthdayConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.khloptsev.hibernate.starter.entity.Birthday;
 import ru.khloptsev.hibernate.starter.entity.Role;
 import ru.khloptsev.hibernate.starter.entity.User;
+import ru.khloptsev.hibernate.starter.utils.HibernateUtils;
 
 import java.time.LocalDate;
 
 public class HibernateRunner {
+    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
+
     static void main() {
-        Configuration configuration = new Configuration();
-        configuration.configure();
+        log.info("Приложение запущено");
+        User user = User.builder()
+                .email("testuser@example.ex")
+                .firstname("test")
+                .lastname("user")
+                .birthDate(new Birthday(LocalDate.of(1986, 1, 1)))
+                .role(Role.ADMIN)
+                .build();
+        log.info("Создана сущность user статус transient: {}", user);
+        try (SessionFactory sessionFactory = HibernateUtils.buildSessionFactory()) {
 
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
-            User user = User.builder()
-                    .email("testuser@example.ex")
-                    .firstname("test")
-                    .lastname("user")
-                    .birthDate(new Birthday(LocalDate.of(1986, 1, 1)))
-                    .role(Role.ADMIN)
-                    .build();
+            try (Session session = sessionFactory.openSession()) {
+                log.info("Открыта сессия: {}", session);
+                session.beginTransaction();
+                user.setFirstname("Oleg");
+                log.warn("User firstname изменено {}", user);
+                System.out.println(session.isDirty());
+                session.find(User.class, "testuser@example.ex");
+//                session.persist(user);
 
-            session.beginTransaction();
-
-            session.persist(user);
-
-            session.beginTransaction().commit();
-
+                log.debug("User: {}, Session: {}", user, session);
+                session.beginTransaction().commit();
+            }
+        } catch (Exception e) {
+            log.error("Произошла ошибка: ", e);
+            throw e;
         }
+        log.info("Приложение завершило работу\n----------------");
     }
 }
