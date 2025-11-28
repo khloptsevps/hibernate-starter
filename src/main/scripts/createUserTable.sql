b53440153f42880a95481924e279814ee7e2d9c9
Petr Khloptsev <p.khloptsev@gmail.com>
2025-11-28 21:21:16 +1000

    Обновил скрипт sql

--- src/main/scripts/createUserTable.sql
+++ src/main/scripts/createUserTable.sql
@@ -1,11 +1,19 @@
 drop table if exists users;
+drop table if exists company;
 
 CREATE TABLE users
 (
-    email VARCHAR(128) PRIMARY KEY ,
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(128) UNIQUE NOT NULL,
     firstname VARCHAR(128),
     lastname VARCHAR(128),
     birth_date DATE,
     age INT,
     role VARCHAR(24)
+);
+
+CREATE TABLE company
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(128) UNIQUE NOT NULL
 );
