96b5a5e90836b640293ebc99ec08a5f97d5bcc58
Petr Khloptsev <p.khloptsev@gmail.com>
2025-11-28 10:26:31 +1000

    Добавил embedded-класс в User

---
+++ src/main/java/ru/khloptsev/hibernate/starter/entity/PersonalInfo.java
@@ -0,0 +1,41 @@
+package ru.khloptsev.hibernate.starter.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.khloptsev.hibernate.starter.converter.BirthdayConverter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Embeddable
+public class PersonalInfo {
+    private String firstname;
+    private String lastname;
+    @Column(name = "birth_date")
+    @Convert(converter = BirthdayConverter.class)
+    private Birthday birthDate;
+    private int age;
+
+    @PrePersist
+    @PreUpdate
+    private void calculateAgeBeforeSave() {
+        if (birthDate == null) {
+            age = 0;
+        } else {
+            age = birthDate.getAge();
+        }
+    }
+
+    @PostLoad
+    private void calculateAgeAfterLoad() {
+        if (birthDate == null) {
+            age = 0;
+        } else {
+            age = birthDate.getAge();
+        }
+    }
+}
